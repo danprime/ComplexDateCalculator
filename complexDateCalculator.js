8038fb72dec896547e31cc3b0577e6a41acfbaa5
Daniel Tse <danprime@gmail.com>
2015-03-23 15:50:22 -0600

    Add documentation about year parameter being optional.

--- complexDateCalculator.js
+++ complexDateCalculator.js
@@ -6,6 +6,7 @@
  * dayOfWeek (1 = Monday, 2 = Tuesday, ... ,7 = Sunday)
  * iterator (1 = First, 2 = Second, 3 = Third )
  * month (1 = January, 2 = February, ... , 12 = December)
+ * year - the year you want to calculate from (optional, if null, defaults to current year)
  *
  * Example Usage:
  * Calculate the Labour Day (First Monday of September) = complexDateCalculator(1, 1, 9, 2015)
